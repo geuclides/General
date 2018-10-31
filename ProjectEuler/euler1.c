bf1ffd516d061e00d4976b8ef78317c93ccfe2c4
Gustavo Euclides <44531041+geuclides@users.noreply.github.com>
2018-10-31 10:06:00 -0300

    Add files via upload

---
+++ ProjectEuler/euler1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int soma = 0;
+	for (int i = 0; i < 1000; i++) {
+		if ((i % 3) == 0 || (i % 5) == 0) {
+			soma = i + soma;
+		}
+	}
+	printf("%d\n", soma);
+	//result: 233168
+}
